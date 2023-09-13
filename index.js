432d016ee237d225ce40ffec766f382fdbe0615a
Jangra9729 <144818953+Jangra9729@users.noreply.github.com>
2023-09-13 21:09:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,18 +27,13 @@ this.style.color= "green";
      break;
 
      case "s":
-        var tom3 = new Audio("sounds/Tom-3.mp3");
+        var tom4 = new Audio("sounds/Tom-3.mp3");
         tom3.play();
      break;
 
-     case "d":
-                function() {
-                var audio = new Audio("../../media/KR881.mp3");
-                audio.play();
-            }
-         
-        // var tom4 = new Audio("sounds/Tom-4.mp3");
-        // tom4.play();
+     case "d": 
+        var tom4 = new Audio("sounds/Tom-4.mp3");
+        tom4.play();
      break;  
 
      case "j":
