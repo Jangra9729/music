6229e90643358da53b6df14324826983339f4078
Jangra9729 <144818953+Jangra9729@users.noreply.github.com>
2023-09-13 21:44:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,18 +22,18 @@ this.style.color= "green";
      break;
     
      case "a":    
-         var tom2 = new Audio("sounds/tom-2.mp3");
-         tom2.play();
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
      break;
 
      case "s":
-           var tom3 = new Audio("sounds/tom-3.mp3");
-           tom3.play();
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
      break;
 
      case "d": 
-          var tom4 = new Audio("sounds/tom-4.mp3");
-          tom4.play();
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
      break;  
 
      case "j":
